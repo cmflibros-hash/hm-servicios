917a8c54829aa55b096a1dbabc97239ca32df2fe
César Muñoz <cmf.libros@gmail.com>
2026-06-18 15:01:14 -0400

    Initial commit - HM Servicios Hola Mundo

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="app">
+      <header className="app-header">
+        <h1>¡Hola Mundo!</h1>
+        <p>HM Servicios - Sistema de Gestión de Bodega</p>
+        <p className="subtitle">Proximamente: Control de stock, alertas por correo y más...</p>
+      </header>
+    </div>
+  )
+}
+
+export default App
